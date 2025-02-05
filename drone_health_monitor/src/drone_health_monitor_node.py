06d43508ae643cb3736a17180e159b1865246380
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-02-05 15:47:44 -0500

    working on drone health node

--- drone_health_monitor/src/drone_health_monitor_node.py
+++ drone_health_monitor/src/drone_health_monitor_node.py
@@ -3,6 +3,7 @@
 import rospy
 from drone_health_monitor.msg import DroneHealth
 from mavros_msgs.msg import State
+from sensor_msgs.msg import NavSatFix, BatteryState
 
 class DroneHealthMonitor():
     def __init__(self):
@@ -15,14 +16,24 @@ class DroneHealthMonitor():
         self.drone_health = DroneHealth()
         
         rospy.Subscriber("/mavros/state", State, self.state_callback)
+        rospy.Subscriber("/mavros/global_position/raw/fix", NavSatFix, self.global_position_callback)
+        rospy.Subscriber("/mavros/battery", BatteryState, self.battery_state_callback)
 
     def state_callback(self, msg: State) -> None:
-        self.drone_health.system_ok = msg.connected
-        self.drone_health.system_mode = msg.mode
-        self.drone_health.fcu_connected = msg.connected
+        self.drone_health.header = msg.header
+        self.drone_health.is_armed = msg.armed
+        self.drone_health.is_connected = msg.connected
+        self.drone_health.flight_mode = msg.mode
+
+    def global_position_callback(self, msg: NavSatFix):
+        self.drone_health.gps_ok = msg.status.status >= msg.status.STATUS_FIX
+
+    def battery_state_callback(self, msg: BatteryState):
+        self.drone_health.battery_percent = msg.percentage
+        self.drone_health.battery_voltage = msg.voltage
 
     def display_drone_health(self) -> None:
-        rospy.loginfo(f"Drone FCU Connected: { self.drone_health.fcu_connected }")
+        rospy.loginfo(f"Drone Battery Connected: { self.drone_health.battery_percent }")
 
     def start(self) -> None:
         while not rospy.is_shutdown():
