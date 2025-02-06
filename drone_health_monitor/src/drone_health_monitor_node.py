e1efe337155fbae3cc499c0be2e1087593b219c0
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-02-06 14:36:26 -0500

    finished basic drone health

--- drone_health_monitor/src/drone_health_monitor_node.py
+++ drone_health_monitor/src/drone_health_monitor_node.py
@@ -3,21 +3,25 @@
 import rospy
 from drone_health_monitor.msg import DroneHealth
 from mavros_msgs.msg import State
-from sensor_msgs.msg import NavSatFix, BatteryState
+from sensor_msgs.msg import NavSatFix, BatteryState, Temperature
 
 class DroneHealthMonitor():
     def __init__(self):
         rospy.init_node("drone_health_monitor")
-
-        # Print at 1 Hz looped
-        self.rate = 1.0
-        self.loop_rate = rospy.Rate(self.rate)
 
         self.drone_health = DroneHealth()
         
         rospy.Subscriber("/mavros/state", State, self.state_callback)
         rospy.Subscriber("/mavros/global_position/raw/fix", NavSatFix, self.global_position_callback)
         rospy.Subscriber("/mavros/battery", BatteryState, self.battery_state_callback)
+        rospy.Subscriber("/mavros/imu/temperature", Temperature, self.imu_temperature_callback)
+
+        # Publisher to /drone_health
+        self.pub = rospy.Publisher("/drone_health", DroneHealth, queue_size=10)
+
+        # Print at 10 Hz looped
+        self.rate = 10
+        self.loop_rate = rospy.Rate(self.rate)
 
     def state_callback(self, msg: State) -> None:
         self.drone_health.header = msg.header
@@ -32,12 +36,17 @@ class DroneHealthMonitor():
         self.drone_health.battery_percent = msg.percentage
         self.drone_health.battery_voltage = msg.voltage
 
+    def imu_temperature_callback(self, msg: Temperature):
+        self.drone_health.temperature = msg.temperature
+
+    # This is for debugging while developing
     def display_drone_health(self) -> None:
-        rospy.loginfo(f"Drone Battery Connected: { self.drone_health.battery_percent }")
+        rospy.loginfo(f"{ self.drone_health }")
 
     def start(self) -> None:
         while not rospy.is_shutdown():
-            self.display_drone_health()
+            self.drone_health.header.stamp = rospy.Time.now()
+            self.pub.publish(self.drone_health)
             self.loop_rate.sleep()
 
 
